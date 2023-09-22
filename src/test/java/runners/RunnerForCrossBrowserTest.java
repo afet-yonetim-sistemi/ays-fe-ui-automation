8a4481f4ea5ab99db4bf989f59648f6322161794
ucsengun <sutukosa@protonmail.com>
2023-09-22 08:21:11 -0500

    .....

---
+++ src/test/java/runners/RunnerForCrossBrowserTest.java
@@ -0,0 +1,35 @@
+package runners;
+
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import utilities.DriverClass;
+
+@CucumberOptions(
+        tags = "@Login",
+        features = {"src/test/java/features"},
+        glue = {"step_definitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+
+
+public class RunnerForCrossBrowserTest extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    @Parameters
+    public void beforeClass(String browserName){DriverClass.setThreadDriverName(browserName);
+    }
+
+    @AfterClass
+    public static void writeExtentReport(){
+        ExtentService.getInstance().setSystemInfo("Windows User Name",System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone",System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name","Team7");
+        ExtentService.getInstance().setSystemInfo("Application Name","EU");
+        ExtentService.getInstance().setSystemInfo("Operating System Info",System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department","QA");
+    }
+}
---
+++ src/test/java/runners/RunnerForParallelTest.java
@@ -0,0 +1,27 @@
+package runners;
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+public class RunnerForParallelTest  extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    @Parameters(value = "browsers")
+    public void beforeClass(String browserName) {DriverClass.setThreadDriverName(browserName);
+    }
+
+    @AfterClass
+    public static void writeExtentReport(){
+        ExtentService.getInstance().setSystemInfo("Windows User Name",System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone",System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name","Team7");
+        ExtentService.getInstance().setSystemInfo("Application Name","Enuygun.com");
+        ExtentService.getInstance().setSystemInfo("Operating System Info",System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department","QA");
+
+
+    }
+}
