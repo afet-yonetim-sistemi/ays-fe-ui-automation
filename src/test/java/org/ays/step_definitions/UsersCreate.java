abbd2ed76cf48209b05b3eae1c6d0700a7f86107
Agit Rubar Demir <demiragitrubar@gmail.com>
2024-02-18 15:54:53 +0300

    AYS-157 | Generate Random Methods Have Been Separated with `AysRandomUtil` Class from `MyMethod` Class

--- src/test/java/org/ays/step_definitions/UsersCreate.java
+++ src/test/java/org/ays/step_definitions/UsersCreate.java
@@ -2,11 +2,12 @@ package org.ays.step_definitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
 import org.ays.pages.UsersPOM;
+import org.ays.utilities.AysRandomUtil;
 import org.ays.utilities.MyMethods;
+import org.testng.Assert;
 
-public class UsersCreate extends MyMethods{
+public class UsersCreate extends MyMethods {
 
     UsersPOM usersPOM = new UsersPOM();
 
@@ -19,39 +20,40 @@ public class UsersCreate extends MyMethods{
     @When("Fill out the users form")
     public void fill_out_the_users_form() {
 
-        usersPOM.sendKeysMethod(usersPOM.getFirstName(),generateRandomName());
-        usersPOM.sendKeysMethod(usersPOM.getLastName(),generateRandomSurname());
+        usersPOM.sendKeysMethod(usersPOM.getFirstName(), AysRandomUtil.generateFirstName());
+        usersPOM.sendKeysMethod(usersPOM.getLastName(), AysRandomUtil.generateLastName());
         usersPOM.clickMethod(usersPOM.getCountryCode());
         usersPOM.clickMethod(usersPOM.getTurkeyCountryCode());
-        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(),generateRandomPhoneNumber());
-
+        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(), AysRandomUtil.generatePhoneNumber());
     }
+
     @And("{string} success message is displayed")
     public void successMessageIsDisplayed(String message) {
         waitUntilVisible(usersPOM.getSuccessfulMessage());
-        Assert.assertEquals(usersPOM.getSuccessfulMessage().getText(),message);
+        Assert.assertEquals(usersPOM.getSuccessfulMessage().getText(), message);
     }
 
     @And("Enter invalid characters in the name field while filling out the users form")
     public void enterInvalidCharactersInTheNameFieldWhileFillingOutTheUsersForm() {
 
-        usersPOM.sendKeysMethod(usersPOM.getFirstName(),"?--/-**-8965");
-        usersPOM.sendKeysMethod(usersPOM.getLastName(), generateRandomSurname());
+        usersPOM.sendKeysMethod(usersPOM.getFirstName(), "?--/-**-8965");
+        usersPOM.sendKeysMethod(usersPOM.getLastName(), AysRandomUtil.generateLastName());
         usersPOM.clickMethod(usersPOM.getCountryCode());
         usersPOM.clickMethod(usersPOM.getTurkeyCountryCode());
-        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(), generateRandomPhoneNumber());
+        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(), AysRandomUtil.generatePhoneNumber());
     }
 
     @And("Enter invalid characters in the last name field while filling out the users form")
     public void enterInvalidCharactersInTheLastNameFieldWhileFillingOutTheUsersForm() {
 
-        usersPOM.sendKeysMethod(usersPOM.getFirstName(),generateRandomName());
-        usersPOM.sendKeysMethod(usersPOM.getLastName(),"?--/-**-8965");
+        usersPOM.sendKeysMethod(usersPOM.getFirstName(), AysRandomUtil.generateFirstName());
+        usersPOM.sendKeysMethod(usersPOM.getLastName(), "?--/-**-8965");
         usersPOM.clickMethod(usersPOM.getCountryCode());
         usersPOM.clickMethod(usersPOM.getTurkeyCountryCode());
-        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(),generateRandomPhoneNumber());
+        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(), AysRandomUtil.generatePhoneNumber());
 
     }
+
     @And("{string} error massage is displayed")
     public void errorMassageIsDisplayed(String message) {
         waitUntilVisible(usersPOM.getErrorMassage());
@@ -61,17 +63,16 @@ public class UsersCreate extends MyMethods{
     @And("Enter invalid characters in the phone number field while filling out the users form")
     public void enterInvalidCharactersInThePhoneNumberFieldWhileFillingOutTheUsersForm() {
 
-        usersPOM.sendKeysMethod(usersPOM.getFirstName(),generateRandomName());
-        usersPOM.sendKeysMethod(usersPOM.getLastName(),generateRandomSurname());
+        usersPOM.sendKeysMethod(usersPOM.getFirstName(), AysRandomUtil.generateFirstName());
+        usersPOM.sendKeysMethod(usersPOM.getLastName(), AysRandomUtil.generateLastName());
         usersPOM.clickMethod(usersPOM.getCountryCode());
         usersPOM.clickMethod(usersPOM.getTurkeyCountryCode());
-        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(),"abc");
+        usersPOM.sendKeysMethod(usersPOM.getPhoneNumber(), "abc");
     }
 
     @And("{string} massage is displayed")
     public void massageIsDisplayed(String message) {
-        Assert.assertEquals(usersPOM.getInvalidPhoneNumberMessage().getText(),message);
+        Assert.assertEquals(usersPOM.getInvalidPhoneNumberMessage().getText(), message);
     }
-
 
 }
