89a94a2894b0e1f61fb20a793b1ab226771171b5
Agit Rubar Demir <demiragitrubar@gmail.com>
2024-02-19 11:13:14 +0300

    AYS-157 | Read Logic of Configurations Has Been Refactored and Code Refactor

--- src/test/java/org/ays/step_definitions/Login.java
+++ src/test/java/org/ays/step_definitions/Login.java
@@ -5,24 +5,25 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.ays.pages.LoginPOM;
-import org.ays.utilities.ConfigurationReader;
+import org.ays.utilities.AysConfigurationProperty;
 import org.ays.utilities.DriverUtil;
 import org.ays.utilities.PageActions;
 import org.testng.Assert;
 
 public class Login extends PageActions {
+
     LoginPOM loginPOM = new LoginPOM();
 
     @Given("Access the AYOS system")
     public void access_the_AYOS_system() {
-        DriverUtil.generateDriver().get(ConfigurationReader.getProperty("api.url"));
+        DriverUtil.generateDriver().get(AysConfigurationProperty.Api.URL);
         DriverUtil.generateDriver().manage().window().maximize();
     }
 
     @When("Enter the username and password")
     public void enter_the_username_and_password() {
-        this.sendKeysMethod(loginPOM.getLoginUsername(), ConfigurationReader.getProperty("admin-user.one.username"));
-        this.sendKeysMethod(loginPOM.getLoginPassword(), ConfigurationReader.getProperty("admin-user.one.password"));
+        this.sendKeysMethod(loginPOM.getLoginUsername(), AysConfigurationProperty.InstitutionOne.AdminUserOne.USERNAME);
+        this.sendKeysMethod(loginPOM.getLoginPassword(), AysConfigurationProperty.InstitutionOne.AdminUserOne.PASSWORD);
     }
 
     @And("Click the Login button")
@@ -35,4 +36,5 @@ public class Login extends PageActions {
         this.waitUntilVisible(loginPOM.getAdminsHeader());
         Assert.assertTrue(loginPOM.getAdminsHeader().isDisplayed());
     }
+
 }
