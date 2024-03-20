a4cf689130c89aff8ee63a7d2506f47162f6a2b2
cglais <caglaataseven1988@gmail.com>
2024-03-20 21:02:40 +0300

    AYS-191 | Cucumber report file added.

--- src/test/java/org/ays/runners/RunnerForRegressionTest.java
+++ src/test/java/org/ays/runners/RunnerForRegressionTest.java
@@ -2,6 +2,6 @@ package org.ays.runners;
 
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@Regression")
+@CucumberOptions(tags = "@Regression",publish = true)
 public class RunnerForRegressionTest extends AysAbstractTestNGCucumberTests {
 }
