ad1c96042170d7f9897249de23a80ec6f62dd2a4
cglais <caglaataseven1988@gmail.com>
2024-03-20 21:35:02 +0300

    AYS-191 | Super Admin Information added to yml file

--- src/test/java/org/ays/runners/RunnerForRegressionTest.java
+++ src/test/java/org/ays/runners/RunnerForRegressionTest.java
@@ -2,6 +2,9 @@ package org.ays.runners;
 
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@Regression",publish = true)
+@CucumberOptions(
+        plugin = {"pretty"},
+        tags = "@Regression", publish = true
+)
 public class RunnerForRegressionTest extends AysAbstractTestNGCucumberTests {
 }
