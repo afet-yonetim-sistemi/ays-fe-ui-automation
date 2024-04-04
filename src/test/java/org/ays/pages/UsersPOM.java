dd9a2348f41b5ded838855c3006159ef9f4f3101
cglais <caglaataseven1988@gmail.com>
2024-04-04 17:14:51 +0300

    AYS-206 | `sureDeleteButton` locator has been changed.

--- src/test/java/org/ays/pages/UsersPOM.java
+++ src/test/java/org/ays/pages/UsersPOM.java
@@ -69,7 +69,7 @@ public class UsersPOM {
     @FindBy(xpath = "(//tr[contains(@class,'ant-table-row-level-0')])[1]/td/div/div/button")
     private WebElement deleteUsersIcon;
 
-    @FindBy(xpath = "//span[text()='Delete']")
+    @FindBy(xpath = "//button/span[.='Delete']")
     private WebElement sureDeleteButton;
 
     @FindBy(xpath = "//div[@class='ant-notification-notice-message']")
